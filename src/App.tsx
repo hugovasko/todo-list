54f3078beb3e3738e9ea7e97ab0680914825bd25
Vasko Nikolov <100063929+hugovasko@users.noreply.github.com>
2022-12-09 21:27:51 +0200

    Todo-4: Init tailwind css

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function App() {
   return (
-    <div>
+    <div className="text-3xl font-bold underline m-6">
       <p>Hello React!</p>
       <p>What's up?</p>
     </div>
